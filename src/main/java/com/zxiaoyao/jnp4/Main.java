1082265318103de2705b2d16872967b01da03c97
hlantian <zxiaoyao313@163.com>
2021-01-29 14:27:30 +0800

    init

---
+++ src/main/java/com/zxiaoyao/jnp4/Main.java
@@ -0,0 +1,12 @@
+package com.zxiaoyao.jnp4;
+
+/**
+ * @Description
+ * @Author hlantian
+ * @Date 2021/1/27 17:51
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello hlantian");
+    }
+}
